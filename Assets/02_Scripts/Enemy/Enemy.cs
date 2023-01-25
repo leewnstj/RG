a7edb4437c1012cc9995c7abe3fea1b19636e2d8
leewnstj <ij35492581@gmail.com>
2023-01-25 22:41:56 +0900

    ww
    ww

--- Assets/02_Scripts/Enemy/Enemy.cs
+++ Assets/02_Scripts/Enemy/Enemy.cs
@@ -4,30 +4,26 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] Vector2 FollowSize;
-    public LayerMask layer;
-    GameObject target;
+
+    [SerializeField] float EnemySpeed;
+    [SerializeField] float FollowDistance;
+    [SerializeField] LayerMask layer;
+
     private void Start()
     {
-        target = GameObject.Find("Player");
-    }
-    private void Update()
-    {
-        PlayerFollow();        
-    }
-    private void PlayerFollow()
-    {
-        Vector2 dir;
-        Collider2D Sense = Physics2D.OverlapBox(transform.position, FollowSize, 0, layer);
-        if (Sense)
-        {
-            dir = transform.position - target.transform.position;
-        }
     }
 
-    private void OnDrawGizmos()
+    private void FixedUpdate()
     {
-        Gizmos.color = Color.green;
-        Gizmos.DrawCube(transform.position, FollowSize);
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.forward, FollowDistance, layer);
+        if(hit.collider != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, hit.collider.transform.position, Time.deltaTime * EnemySpeed);
+        }
     }
 }
