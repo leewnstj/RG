605126b662bc2f2339ea52dd26bf8df5fc9c863a
leewnstj <ij35492581@gmail.com>
2023-01-28 22:37:11 +0900

    EnemyStats
    EnemyStats

--- Assets/02_Scripts/Enemy/EnemyStats.cs
+++ Assets/02_Scripts/Enemy/EnemyStats.cs
@@ -2,24 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStats : Entity
+public class EnemyStats : Entity
 {
+    [SerializeField] float maxHP;
     [SerializeField] float hpRecovery;
     [SerializeField] float maxMP;
     [SerializeField] float mpRecovery;
+
 
     private void Awake()
     {
         base.SetUp();
     }
 
-    //기본 체력 + 스탯 보너스 + 버프 등과 같이 계산
-    public override float MaxHP => MaxHPBasic + MaxHPAttrBonus;
-    //100 + 현재 레벨 * 30
-    public float MaxHPBasic => 100 + 1 * 30;
-    //힘 * 10
-    public float MaxHPAttrBonus => 10 * 10;
-
+    public override float MaxHP => maxHP;
     public override float HPRecovery => hpRecovery;
     public override float MaxMP => maxMP;
     public override float MPRecovery => mpRecovery;
